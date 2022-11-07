08daa57c5d8df460ea13ff790bc4d0e2917b61ef
LaloCode <lalo_pichu22@hotmail.com>
2022-11-06 22:26:08 -0600

    Add initial pagination template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 import { AppComponent } from './app.component';
+import { PaginationComponent } from './pagination/pagination.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, MatToolbarModule],
+  declarations: [AppComponent, PaginationComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatToolbarModule, MatPaginatorModule],
   providers: [],
   bootstrap: [AppComponent],
 })
